5a5b39281111cfd6c239fb0a8a8886ad6404fcbe
Azizbek <azizbek2727@bk.ru>
2025-11-24 16:39:17 +0500

    showprice

--- common/widgets/ShowPrice.php
+++ common/widgets/ShowPrice.php
@@ -15,7 +15,7 @@ class ShowPrice extends \dvizh\shop\widgets\ShowPrice
 
         return Html::tag(
             $this->htmlTag,
-            $this->model->getPrice() . ' ' . Setting::findOne(['key' => 'currency'])->value ?? '',
+            (Setting::findOne(['key' => 'currency'])->value ?? '') . $this->model->getPrice(),
             ['class' => "dvizh-shop-price dvizh-shop-price-{$this->model->id} {$this->cssClass}"]
         );
     }
