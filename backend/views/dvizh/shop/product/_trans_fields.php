e4a01317212c7aa31b1446e683e065726b04c50a
Azizbek <azizbek2727@bk.ru>
2025-12-02 18:12:50 +0500

    Product

--- backend/views/dvizh/shop/product/_trans_fields.php
+++ backend/views/dvizh/shop/product/_trans_fields.php
@@ -1,17 +1,68 @@
 <?php
 /** @var $model \yii\db\ActiveRecord */
-/** @var $lang string */
 
-$attrs = $model->getTranslatableAttributes();
+use common\models\Translation;
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
-foreach ($attrs as $attr): ?>
-    <div class="form-group">
-        <label><?= $model->getAttributeLabel($attr) ?></label>
+$attrs   = $model->getTranslatableAttributes();
+$widgets = method_exists($model, 'getTranslationWidgets')
+    ? $model->getTranslationWidgets()
+    : [];
+foreach ($attrs as $attr):
 
-        <?= \yii\helpers\Html::textarea(
-            "Translation[{$lang}][{$attr}]",
-            \common\models\Translation::findValue(get_class($model), $model->id, $attr, $lang),
-            ['class' => 'form-control']
-        ) ?>
-    </div>
-<?php endforeach; ?>
+    $widget = $widgets[$attr] ?? 'textarea';
+    $value  = \common\models\Translation::findValue(get_class($model), $model->id, $attr, $lang);
+
+    echo "<div class='form-group'>";
+    echo "<label>" . Html::encode($model->getAttributeLabel($attr)) . "</label>";
+
+    switch ($widget) {
+
+        case 'input':
+            echo Html::textInput("Translation[{$lang}][{$attr}]", $value, [
+                'class' => 'form-control'
+            ]);
+            break;
+
+        case 'imperavi':
+            echo \vova07\imperavi\Widget::widget([
+                'name'  => "Translation[{$lang}][{$attr}]",
+                'value' => $value,
+                'settings' => [
+                    'minHeight' => 200,
+                    'plugins'   => ['fullscreen', 'fontcolor', 'video'],
+                ],
+//                'plugins' => ['fullscreen', 'fontcolor', 'video'],
+                'options'=>[
+                    'minHeight' => 400,
+                    'maxHeight' => 400,
+                    'buttonSource' => true,
+                    'imageUpload' => Url::toRoute(['tools/upload-imperavi'])
+                ],
+            ]);
+            break;
+
+        case 'ckeditor':
+            echo \dosamigos\ckeditor\CKEditor::widget([
+                'name'  => "Translation[{$lang}][{$attr}]",
+                'value' => $value,
+            ]);
+            break;
+
+        default:
+        case 'textarea':
+            echo Html::textarea("Translation[{$lang}][{$attr}]", $value, [
+                'class' => 'form-control',
+                'rows' => 4
+            ]);
+            break;
+    }
+
+    echo "</div>";
+
+endforeach;
+
+
+?>
--- common/models/dvizh/Product.php
+++ common/models/dvizh/Product.php
@@ -12,12 +12,37 @@ class Product extends \dvizh\shop\models\Product
 
     public function getTranslatableAttributes()
     {
-        return ['name'];
+        return [
+            'name',
+            'short_text',
+            'text',
+        ];
+    }
+
+    public function getTranslationWidgets()
+    {
+        return [
+            'name' => 'input',        // simple text input
+            'short_text' => 'textarea',
+            'text' => 'imperavi',     // full WYSIWYG
+        ];
     }
 
     // convenience getters used by views (so $product->name works unchanged)
     public function getName()
     {
         return $this->tOrOrig('name');
+    }
+
+    // convenience getters used by views (so $product->name works unchanged)
+    public function getShort_text()
+    {
+        return $this->tOrOrig('short_text');
+    }
+
+    // convenience getters used by views (so $product->name works unchanged)
+    public function getText()
+    {
+        return $this->tOrOrig('text');
     }
 }
